5a1d5051780d72a0df533bb191b0e2ef92a0246c
ALAN GABRIEL DAVILA RISCO <ustargab@gmail.com>
2015-04-06 00:25:39 +0200

    i18n

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 'use strict';
 // Here we set up an angular module. We'll attach controllers and
 // other components to this module.
-angular.module('admongo', ['ui.bootstrap']).config(function ($routeProvider) {
-	$routeProvider.when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'}).otherwise({redirectTo: '/'});
-});
+angular.module('admongo', ['ui.bootstrap',
+                           'pascalprecht.translate']).
+                           config('$routeProvider', function ($routeProvider) {
+                        	   $routeProvider.when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'}).
+                        	   otherwise({redirectTo: '/'});
+                           }).
+                           config('$translateProvider', function($translateProvider) {
+                        	   $translateProvider.useStaticFilesLoader({
+                        		   prefix: '/i18n/',
+                        		   suffix: '.json'
+                        		 });
+                        	});
