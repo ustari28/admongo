2ebb761c77a0997b56cd37885eb29f363e5dcb2e
ALAN GABRIEL DAVILA RISCO <ustargab@gmail.com>
2015-04-07 12:01:42 +0200

    fix 1

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ angular.module('admongo', ['ui.bootstrap',
                            }).
                            config('$translateProvider', function($translateProvider) {
                         	   $translateProvider.useStaticFilesLoader({
-                        		   prefix: '/i18n/',
+                        		   prefix: 'i18n/',
                         		   suffix: '.json'
                         		 });
+                        	   $translateProvider.determinePreferredLanguage(function() {
+                        		   // Chrome devuelve "es" y IE y Firefox devuelven "es-ES" por lo que nos quedamos con los dos primeros caracteres para
+                        		   // seleccionar idioma
+	                        	   var userLang = navigator.language || navigator.userLanguage;
+	                        	   var subs = userLang.substring(0,2);
+	                        	   return subs;
+                        		   // return navigator.language.slice(0,2);
+                        	   });
+                        	   $translateProvider.fallbackLanguage('es');
                         	});
